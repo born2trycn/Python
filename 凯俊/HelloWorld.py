c59b602ba4bb1498eaa67973e1c9d71faeb54d81
yuzhaohai <yuzhaohai@126.com>
2019-11-22 17:55:19 +0800

    20191116

---
+++ 凯俊/HelloWorld.py
@@ -0,0 +1,3 @@
+print('同学们，大家好，欢迎来到编程猫！')
+
+print('hello, world')
---
+++ 凯俊/正方形.py
@@ -0,0 +1,21 @@
+import turtle
+
+mikey = turtle.Pen()
+
+mikey.shape('turtle')
+
+mikey.forward(200)
+
+mikey.left(90)
+
+mikey.forward(200)
+
+mikey.left(90)
+
+mikey.forward(200)
+
+mikey.left(90)
+
+mikey.forward(200)
+
+turtle.done()
