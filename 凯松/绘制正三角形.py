0a5d1fc326db81e6488dd7e0056029b8cba17be8
yuzhaohai <yuzhaohai@126.com>
2019-11-22 16:57:43 +0800

    20191116

---
+++ 凯松/绘制正三角形.py
@@ -0,0 +1,17 @@
+import turtle
+
+steve = turtle.Pen()
+
+steve.shape('turtle')
+
+steve.forward(100)
+
+steve.left(120)
+
+steve.forward(100)
+
+steve.left(120)
+
+steve.forward(100)
+
+turtle.done()
